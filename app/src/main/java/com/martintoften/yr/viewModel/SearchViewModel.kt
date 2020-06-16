1fc0a0cf4fc44251b62422f0e762f78e19acbcbf
Martin <martin@bakkenbaeck.no>
2020-06-16 15:02:27 +0200

    Add debounce to search

--- app/src/main/java/com/martintoften/yr/viewModel/SearchViewModel.kt
+++ app/src/main/java/com/martintoften/yr/viewModel/SearchViewModel.kt
@@ -2,11 +2,43 @@ package com.martintoften.yr.viewModel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+private const val DEBOUNCE_DELAY = 400L
 
 class SearchViewModel : ViewModel() {
 
+    private val channel = ConflatedBroadcastChannel<String>()
+
+    init {
+        initSearchListener()
+    }
+
+    private fun initSearchListener() {
+        viewModelScope.launch {
+            channel.asFlow()
+                .debounce(DEBOUNCE_DELAY)
+                .map { it + "1" }
+                .flowOn(Dispatchers.IO)
+                .collect { handleSearchResult(it) }
+        }
+    }
+
+    private fun handleSearchResult(result: String) {
+        Log.d("Search result", result)
+    }
+
     fun search(value: String) {
-        Log.d("search", value)
-        // TODO: Search for location
+        viewModelScope.launch {
+            channel.send(value)
+        }
     }
 }
