b6767b732d0309c1c5704bfaec97a22260295f18
Martin <martin@bakkenbaeck.no>
2020-06-18 12:49:44 +0200

    Ignore unknown keys in json

--- app/src/main/java/com/martintoften/yr/network/Api.kt
+++ app/src/main/java/com/martintoften/yr/network/Api.kt
@@ -2,11 +2,13 @@ package com.martintoften.yr.network
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
 import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
 import retrofit2.Retrofit
 import java.io.File
 import java.util.concurrent.TimeUnit
@@ -17,15 +19,22 @@ private const val CACHE_SIZE = 5L * 1024L * 1024L
 fun buildYrApi(cacheDir: File): YrApi {
     val interceptors = listOf(getLoggingInterceptor())
     val client = getHttpClient(cacheDir, interceptors)
-    val contentType = "application/json".toMediaType()
+    val converterFactory = buildConverterFactory()
 
     val retrofit = Retrofit.Builder()
         .client(client.build())
         .baseUrl(SEARCH_API_URL)
-        .addConverterFactory(Json.asConverterFactory(contentType))
+        .addConverterFactory(converterFactory)
         .build()
 
     return retrofit.create(YrApi::class.java)
+}
+
+private fun buildConverterFactory(): Converter.Factory {
+    val contentType = "application/json".toMediaType()
+    val jsonConfiguration = JsonConfiguration(ignoreUnknownKeys = true)
+    val json = Json(jsonConfiguration)
+    return json.asConverterFactory(contentType)
 }
 
 private fun getHttpClient(cacheDir: File, interceptors: List<Interceptor> = emptyList()): OkHttpClient.Builder {
